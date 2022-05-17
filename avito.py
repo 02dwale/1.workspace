20499bf5b07e14699f5d5f406e6278a8e76cf02e
Mark Rudakov <02dwale@gmail.com>
2022-05-17 23:54:34 +0700

    refresh git

--- avito.py
+++ avito.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
-r = requests.get("https://www.avito.ru/tomsk/noutbuki?cd=1&q=huawei+matebook+d16")
+r = requests.get("https://www.avito.ru/rossiya/noutbuki?cd=1&q=huawei+matebook+d16")
 html = BS(r.content, 'html.parser')
 
-
-    title = select('.item-price > price')
+for el in html.select(".items-items-kAJAg > .iva-item-root-_lk9K photo-slider-slider-S15A_ iva-item-list-rfgcH iva-item-redesign-rop6P iva-item-responsive-_lbhG items-item-My3ih items-listItem-Gd1jN js-catalog-item-enum"):
+    title = el.select('.price-price-JP7qe > .price-text-_YGDY text-text-LurtD text-size-s-BxGpL')
     print( title[0].text )
+    
+    
+    
--- calculator.py
+++ calculator.py
@@ -1,2 +1,11 @@
-x = 5
-name = "Mark"
+import telebot
+
+bot = telebot.TeleBot("5339792476:AAH29caOBZ_imHgS3y1TtjnCjtMNcnmMdhA") #Чтобы получить токен, в телеграме нужно найти бота с ником @BotFather и написать команду /newbot
+
+@bot.message_handler(content_types=['text']) #наша функция реагирует только на один аргумент - текстовое сообщение, на картинки, войсы, стикеры он не реагирует
+def send_echo(message):  #наша функция принимает только один параметр, сообщенеи)
+    #bot.reply_to(message,message.text) #функция ответа
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling( none_stop = True )
+
